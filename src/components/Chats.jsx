6022d1e77df42a9f816cfe56ba521a31db3a654f
TarunCore <112856864+TarunCore@users.noreply.github.com>
2023-05-12 12:48:15 +0530

    file initial#1

---
+++ src/components/Chats.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Chats = () => {
+  return (
+    <div className='chats'>
+        <div className="userChat">
+        <img src="https://feeds.abplive.com/onecms/images/uploaded-images/2023/03/18/f9b18e043a34542febb14a0340408148e357b.jpg?impolicy=abp_cdn&imwidth=650" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+        </div>
+        <div className="userChat">
+        <img src="https://feeds.abplive.com/onecms/images/uploaded-images/2023/03/18/f9b18e043a34542febb14a0340408148e357b.jpg?impolicy=abp_cdn&imwidth=650" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+        </div>
+        <div className="userChat">
+        <img src="https://feeds.abplive.com/onecms/images/uploaded-images/2023/03/18/f9b18e043a34542febb14a0340408148e357b.jpg?impolicy=abp_cdn&imwidth=650" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Chats
